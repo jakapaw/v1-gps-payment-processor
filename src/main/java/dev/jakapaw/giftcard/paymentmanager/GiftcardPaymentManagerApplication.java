dde0fb292e53472cf07af97083019ca51f1bc78a
Jek08 <jk.pratama86@gmail.com>
2024-11-09 21:30:17 +0700

    Initialize project

---
+++ src/main/java/dev/jakapaw/giftcard/paymentmanager/GiftcardPaymentManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.jakapaw.giftcard.paymentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftcardPaymentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftcardPaymentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jakapaw/giftcard/paymentmanager/GiftcardPaymentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jakapaw.giftcard.paymentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftcardPaymentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
