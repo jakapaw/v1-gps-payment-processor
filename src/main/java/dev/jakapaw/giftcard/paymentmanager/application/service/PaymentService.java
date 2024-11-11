abf2b333be58a802f781cbde759d92bd1b525592
Jek08 <jk.pratama86@gmail.com>
2024-11-11 13:03:21 +0700

    Save events to datastore at the end of payment process

--- src/main/java/dev/jakapaw/giftcard/paymentmanager/application/service/PaymentService.java
+++ src/main/java/dev/jakapaw/giftcard/paymentmanager/application/service/PaymentService.java
@@ -1,11 +1,14 @@
 package dev.jakapaw.giftcard.paymentmanager.application.service;
 
+import dev.jakapaw.giftcard.paymentmanager.application.command.InitiatePaymentCommand;
 import dev.jakapaw.giftcard.paymentmanager.application.event.PaymentCompleted;
 import dev.jakapaw.giftcard.paymentmanager.application.event.PaymentDeclined;
+import dev.jakapaw.giftcard.paymentmanager.application.event.PaymentInitiated;
 import dev.jakapaw.giftcard.paymentmanager.common.PaymentEventWrapper;
 import dev.jakapaw.giftcard.paymentmanager.domain.Payment;
 import dev.jakapaw.giftcard.paymentmanager.domain.PaymentStatus;
 import dev.jakapaw.giftcard.paymentmanager.infrastructure.broker.KafkaProducer;
+import dev.jakapaw.giftcard.paymentmanager.infrastructure.repository.PaymentEventDatastore;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationEventPublisherAware;
 import org.springframework.scheduling.annotation.Async;
@@ -17,30 +20,39 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/*
+Payment Service contains all business operation of payment processing
+ */
+
 @Service
 public class PaymentService implements ApplicationEventPublisherAware {
 
     ApplicationEventPublisher applicationEventPublisher;
     KafkaProducer kafkaProducer;
     PaymentOrchestrator paymentOrchestrator;
+    PaymentEventDatastore paymentEventDatastore;
 
-    public PaymentService(KafkaProducer kafkaProducer, PaymentOrchestrator paymentOrchestrator) {
+    public PaymentService(KafkaProducer kafkaProducer, PaymentOrchestrator paymentOrchestrator, PaymentEventDatastore paymentEventDatastore) {
         this.kafkaProducer = kafkaProducer;
         this.paymentOrchestrator = paymentOrchestrator;
+        this.paymentEventDatastore = paymentEventDatastore;
     }
 
     @Async
-    public CompletableFuture<Payment> initiatePayment(String giftcardSerialNumber, String merchantId, double billed) {
-        String paymentId = generatePaymentId(merchantId);
+    public CompletableFuture<Payment> initiatePayment(InitiatePaymentCommand command) {
+        String paymentId = generatePaymentId(command.getMerchantId());
         Payment payment = new Payment(
                 paymentId,
-                giftcardSerialNumber,
-                merchantId,
-                billed,
+                command.getGiftcardSerialNumber(),
+                command.getMerchantId(),
+                command.getBillAmount(),
                 LocalDateTime.now(),
                 PaymentStatus.ON_VERIFICATION
         );
         kafkaProducer.publishVerificationEvent(payment);
+
+        PaymentInitiated paymentInitiated = new PaymentInitiated(payment);
+        applicationEventPublisher.publishEvent(new PaymentEventWrapper<>(this, paymentInitiated));
 
         CompletableFuture<Payment> completableFuture = new CompletableFuture<>();
         try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
@@ -52,6 +64,8 @@ public class PaymentService implements ApplicationEventPublisherAware {
                     } catch (InterruptedException ignored) {}
                 };
                 Payment paymentFinished = paymentOrchestrator.removeOngoingPayment(paymentId);
+                paymentEventDatastore.saveAll(paymentFinished.getPaymentEvents());
+                paymentFinished.clearEvents();
                 completableFuture.complete(paymentFinished);
             });
         }
