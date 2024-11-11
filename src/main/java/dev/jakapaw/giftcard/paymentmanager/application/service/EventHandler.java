9f74e2f2f9cf2ba1bcb7b2b18eb5c3dbf173521f
Jek08 <jk.pratama86@gmail.com>
2024-11-11 13:06:21 +0700

    Add listener for PaymentInitiated event

---
+++ src/main/java/dev/jakapaw/giftcard/paymentmanager/application/service/EventHandler.java
@@ -0,0 +1,28 @@
+package dev.jakapaw.giftcard.paymentmanager.application.service;
+
+import dev.jakapaw.giftcard.paymentmanager.application.event.PaymentEvent;
+import dev.jakapaw.giftcard.paymentmanager.application.event.PaymentInitiated;
+import dev.jakapaw.giftcard.paymentmanager.infrastructure.broker.KafkaProducer;
+import dev.jakapaw.giftcard.paymentmanager.infrastructure.repository.PaymentEventDatastore;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventHandler {
+
+    PaymentEventDatastore paymentEventDatastore;
+
+    public EventHandler(PaymentEventDatastore paymentEventDatastore) {
+        this.paymentEventDatastore = paymentEventDatastore;
+    }
+
+    @EventListener(PaymentInitiated.class)
+    public void on(PaymentInitiated event) {
+        PaymentEvent paymentEvent = new PaymentEvent(
+                event.getPayment().getPaymentId(),
+                1,
+                event.getPayment()
+        );
+        event.getPayment().addEvent(paymentEvent);
+    }
+}
